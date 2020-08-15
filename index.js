2ff532df490262ebe2fb08a65b4a17f0c077357d
Jude kuti <judekuti@gmail.com>
2020-08-15 07:21:51 -0500

    Expand index file for operations

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
+const app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello world!");
+    res.render('form');
 });
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static('public'));
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.send("recieved your request!");
+});
 app.listen(3000);
