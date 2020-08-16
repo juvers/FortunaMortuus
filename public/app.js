62813e2b0f1eb175155175422db83cbb437637c1
Jude kuti <judekuti@gmail.com>
2020-08-16 00:37:05 -0500

    Detect automatically when browser is back online with sync and retrieve data from indexdb

--- public/app.js
+++ public/app.js
@@ -97,5 +97,15 @@ if ('serviceWorker' in navigator) {
             console.log('ServiceWorker registration failed: ',
                 err);
         });
+        navigator.serviceWorker.ready.then(function(registration) {
+            console.log('Service Worker Ready')
+            return registration.sync.register('sendFormData')
+        }).then(function() {
+            console.log('sync event registered')
+        }).catch(function() {
+            // system was unable to register for a sync,
+            // this could be an OS-level restriction
+            console.log('sync registration failed')
+        });
     });
 }
