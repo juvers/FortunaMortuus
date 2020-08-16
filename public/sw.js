fe484df89a4a32fafad94ca72aa3d5b0ed7c93b0
Jude kuti <judekuti@gmail.com>
2020-08-15 20:58:01 -0500

    Include service workers install fetch and activate events

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,28 @@
+var CACHE_NAME = 'offline-form';
+var urlsToCache = [
+    '/',
+    '/styles.css',
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+
+];
+self.addEventListener('install', function(event) {
+    // install file needed offline
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+self.addEventListener('fetch', function(event) {
+    console.log('Testing fetch in service worker: ',
+        event.request.clone().url)
+
+});
+
+
+
 self.addEventListener('push', event => {
     const data = event.data.json();
 
