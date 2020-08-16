659761040cfe3fc0780f51ad9b188c53e56fa7ae
Jude kuti <judekuti@gmail.com>
2020-08-15 20:22:02 -0500

    Create trigger for push notification

--- index.js
+++ index.js
@@ -54,4 +54,7 @@ app.post('/subscribe', (req, res) => {
         .catch(error => console.error(error));
 });
 
-app.listen(3000);
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
